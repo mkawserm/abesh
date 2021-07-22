ac352c6a3611f2be0a0c4cc8fee528a6662facc3
kawser <mkawserm@gmail.com>
2021-07-22 18:56:29 +0600

    manifest updated

--- model/manifest.go
+++ model/manifest.go
@@ -4,32 +4,28 @@ type AuthorizationManifest struct {
 	Expression string `yaml:"expression" json:"expression"`
 }
 
-type EnvManifest struct {
-	ContractId string                 `yaml:"contract_id" json:"contract_id"`
-	Values     map[string]interface{} `yaml:"values" json:"values"`
-}
-
 type TriggerManifest struct {
-	Trigger string `yaml:"trigger" json:"trigger"`
-	Key     string `yaml:"key" json:"key"`
-	Value   string `yaml:"value" json:"value"`
+	ContractId string `yaml:"contract_id" json:"contract_id"`
+	Key        string `yaml:"key" json:"key"`
+	Value      string `yaml:"value" json:"value"`
 }
 
 type CapabilityManifest struct {
-	ContractId string `yaml:"contract_id" json:"contract_id"`
+	ContractId string                 `yaml:"contract_id" json:"contract_id"`
+	Category   string                 `yaml:"category" json:"category"`
+	Values     map[string]interface{} `yaml:"values" json:"values"`
 }
 
 type ServiceManifest struct {
 	ContractId    string                 `yaml:"contract_id" json:"contract_id"`
 	Runtime       string                 `yaml:"runtime" json:"runtime"`
 	Authorization *AuthorizationManifest `yaml:"authorization" json:"authorization"`
-
-	Triggers     []*TriggerManifest    `yaml:"triggers" json:"triggers"`
-	Capabilities []*CapabilityManifest `yaml:"capabilities" json:"capabilities"`
+	Values        map[string]interface{} `yaml:"values" json:"values"`
+	Triggers      []*TriggerManifest     `yaml:"triggers" json:"triggers"`
+	Capabilities  []*CapabilityManifest  `yaml:"capabilities" json:"capabilities"`
 }
 
 type Manifest struct {
 	Version  string             `yaml:"version" json:"version"`
-	EnvList  []*EnvManifest     `yaml:"env_list" json:"env_list"`
 	Services []*ServiceManifest `yaml:"services" json:"services"`
 }
