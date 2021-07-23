c53672893cb90b1a24e2acfc27896803259e3545
kawser <mkawserm@gmail.com>
2021-07-23 22:20:07 +0600

    all optional field which does not required for running removed from manifest

--- model/manifest.go
+++ model/manifest.go
@@ -7,28 +7,17 @@ type AuthorizationManifest struct {
 }
 
 type TriggerManifest struct {
-	Name          string            `yaml:"name" json:"name"`
-	Version       string            `yaml:"version" json:"version"`
 	ContractId    string            `yaml:"contract_id" json:"contract_id"`
 	TriggerValues map[string]string `yaml:"trigger_values" json:"trigger_values"`
 }
 
 type CapabilityManifest struct {
-	Name       string            `yaml:"name" json:"name"`
-	Version    string            `yaml:"version" json:"version"`
 	ContractId string            `yaml:"contract_id" json:"contract_id"`
-	Source     string            `yaml:"source" json:"source"`
-	Runtime    string            `yaml:"runtime" json:"runtime"` // native or wasm
-	Category   string            `yaml:"category" json:"category"`
 	Values     map[string]string `yaml:"values" json:"values"`
 }
 
 type ServiceManifest struct {
-	Name          string                 `yaml:"name" json:"name"`
-	Version       string                 `yaml:"version" json:"version"`
 	ContractId    string                 `yaml:"contract_id" json:"contract_id"`
-	Source        string                 `yaml:"source" json:"source"`
-	Runtime       string                 `yaml:"runtime" json:"runtime"` // native or wasm
 	Authorization *AuthorizationManifest `yaml:"authorization" json:"authorization"`
 	Values        map[string]string      `yaml:"values" json:"values"`
 	Triggers      []*TriggerManifest     `yaml:"triggers" json:"triggers"`
