12b9a2ce2031780557694b8f162c685c98824dad
kawser <mkawserm@gmail.com>
2021-07-24 18:02:56 +0600

    added comment to Serve of IService interface

--- iface/service.go
+++ iface/service.go
@@ -8,6 +8,10 @@ import (
 type IService interface {
 	ICapability
 
+	// Serve is the entry point of service category capability
+	// ctx is immutable
+	// capabilityRegistry is immutable
+	// event need to respected as immutable
 	Serve(ctx context.Context,
 		capabilityRegistry ICapabilityRegistry,
 		event *model.Event) (*model.Event, error)
