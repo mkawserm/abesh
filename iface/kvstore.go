73c02399d8b1e8e40b9a2a4e491ac96acb675221
kawser <mkawserm@gmail.com>
2021-07-31 15:52:55 +0600

    kv store updated

--- iface/kvstore.go
+++ iface/kvstore.go
@@ -1,4 +1,19 @@
 package iface
 
+import (
+	"context"
+	"time"
+)
+
 type IKVStore interface {
+	ICapability
+
+	// Get the key from store
+	Get(ctx context.Context, key string, value interface{}) error
+
+	// Set the key value data to the store with ttl
+	Set(ctx context.Context, key string, value interface{}, ttl time.Duration) error
+
+	// Delete the key
+	Delete(ctx context.Context, key string) error
 }
