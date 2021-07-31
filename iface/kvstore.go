15b1d4295ac84d7fe857d1c3e485b887546a2367
kawser <mkawserm@gmail.com>
2021-07-31 15:58:26 +0600

    kv store updated

--- iface/kvstore.go
+++ iface/kvstore.go
@@ -2,7 +2,13 @@ package iface
 
 import (
 	"context"
+	"errors"
 	"time"
+)
+
+var (
+	// ErrKeyNotfound need to be returned if the key does not exist
+	ErrKeyNotfound = errors.New("the requested key does not exist")
 )
 
 type IKVStore interface {
