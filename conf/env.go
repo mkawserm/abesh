581157e147ea5cddf412fc834bf0b0248fdbf0a7
kawser <mkawserm@gmail.com>
2021-09-03 01:20:35 +0600

    global request timeout env added

--- conf/env.go
+++ conf/env.go
@@ -3,12 +3,14 @@ package conf
 import (
 	"github.com/caarlos0/env"
 	"sync"
+	"time"
 )
 
 type EnvironmentConfig struct {
-	LogLevel        string `env:"ABESH_LOG_LEVEL" envDefault:"debug"`
-	CMDLogEnabled   bool   `env:"ABESH_CMD_LOG_ENABLED" envDefault:"false"`
-	EventBufferSize int    `env:"ABESH_EVENT_BUFFER_SIZE" envDefault:"100"`
+	LogLevel             string        `env:"ABESH_LOG_LEVEL" envDefault:"debug"`
+	CMDLogEnabled        bool          `env:"ABESH_CMD_LOG_ENABLED" envDefault:"false"`
+	EventBufferSize      int           `env:"ABESH_EVENT_BUFFER_SIZE" envDefault:"100"`
+	GlobalRequestTimeout time.Duration `env:"ABESH_GLOBAL_REQUEST_TIMEOUT" envDefault:"50ms"`
 }
 
 var instantiated *EnvironmentConfig
