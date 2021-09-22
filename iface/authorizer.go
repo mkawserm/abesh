eb39468de215f1d39d4e8fe24cbda100bcf1c494
kawser <mkawserm@gmail.com>
2021-09-22 18:46:31 +0600

    authorizer refactored

--- iface/authorizer.go
+++ iface/authorizer.go
@@ -2,7 +2,11 @@ package iface
 
 import "github.com/mkawserm/abesh/model"
 
+type IIsAuthorized interface {
+	IsAuthorized(expression string, metadata *model.Metadata) bool
+}
+
 type IAuthorizer interface {
 	ICapability
-	IsAuthorized(expression string, metadata *model.Metadata) bool
+	IIsAuthorized
 }
