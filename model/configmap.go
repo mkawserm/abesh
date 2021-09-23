7d3bbe56c3aaea4c841dea480adcd951d7ed810e
kawser <mkawserm@gmail.com>
2021-09-23 21:58:27 +0600

    configmap bug fixed

--- model/configmap.go
+++ model/configmap.go
@@ -47,10 +47,9 @@ func (v ConfigMap) Float32(key string, defaultValue float32) float32 {
 	if o, ok := v[key]; ok {
 		i, err := strconv.ParseFloat(o, 32)
 		if err != nil {
-			return float32(i)
+			return defaultValue
 		}
-
-		return defaultValue
+		return float32(i)
 	}
 	return defaultValue
 }
@@ -59,9 +58,9 @@ func (v ConfigMap) Float64(key string, defaultValue float64) float64 {
 	if o, ok := v[key]; ok {
 		i, err := strconv.ParseFloat(o, 64)
 		if err != nil {
-			return i
+			return defaultValue
 		}
-		return defaultValue
+		return i
 	}
 	return defaultValue
 }
@@ -70,10 +69,10 @@ func (v ConfigMap) Int(key string, defaultValue int) int {
 	if o, ok := v[key]; ok {
 		i, err := strconv.Atoi(o)
 		if err != nil {
-			return i
+			return defaultValue
 		}
 
-		return defaultValue
+		return i
 	}
 	return defaultValue
 }
@@ -82,10 +81,9 @@ func (v ConfigMap) Uint(key string, defaultValue uint) uint {
 	if o, ok := v[key]; ok {
 		i, err := strconv.ParseUint(o, 10, 32)
 		if err != nil {
-			return uint(i)
+			return defaultValue
 		}
-
-		return defaultValue
+		return uint(i)
 	}
 	return defaultValue
 }
@@ -94,10 +92,10 @@ func (v ConfigMap) Int16(key string, defaultValue int16) int16 {
 	if o, ok := v[key]; ok {
 		i, err := strconv.ParseInt(o, 10, 16)
 		if err != nil {
-			return int16(i)
+			return defaultValue
 		}
 
-		return defaultValue
+		return int16(i)
 	}
 	return defaultValue
 }
@@ -106,10 +104,9 @@ func (v ConfigMap) Uint16(key string, defaultValue uint16) uint16 {
 	if o, ok := v[key]; ok {
 		i, err := strconv.ParseUint(o, 10, 16)
 		if err != nil {
-			return uint16(i)
+			return defaultValue
 		}
-
-		return defaultValue
+		return uint16(i)
 	}
 	return defaultValue
 }
@@ -118,10 +115,9 @@ func (v ConfigMap) Int32(key string, defaultValue int32) int32 {
 	if o, ok := v[key]; ok {
 		i, err := strconv.ParseInt(o, 10, 32)
 		if err != nil {
-			return int32(i)
+			return defaultValue
 		}
-
-		return defaultValue
+		return int32(i)
 	}
 	return defaultValue
 }
@@ -130,10 +126,9 @@ func (v ConfigMap) Uint32(key string, defaultValue uint32) uint32 {
 	if o, ok := v[key]; ok {
 		i, err := strconv.ParseUint(o, 10, 32)
 		if err != nil {
-			return uint32(i)
+			return defaultValue
 		}
-
-		return defaultValue
+		return uint32(i)
 	}
 	return defaultValue
 }
@@ -142,9 +137,9 @@ func (v ConfigMap) Int64(key string, defaultValue int64) int64 {
 	if o, ok := v[key]; ok {
 		i, err := strconv.ParseInt(o, 10, 64)
 		if err != nil {
-			return i
+			return defaultValue
 		}
-		return defaultValue
+		return i
 	}
 	return defaultValue
 }
@@ -153,9 +148,9 @@ func (v ConfigMap) Uint64(key string, defaultValue uint64) uint64 {
 	if o, ok := v[key]; ok {
 		i, err := strconv.ParseUint(o, 10, 64)
 		if err != nil {
-			return i
+			return defaultValue
 		}
-		return defaultValue
+		return i
 	}
 	return defaultValue
 }
