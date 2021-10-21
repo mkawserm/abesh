eb845c6f15b187e004929c1771533952c4af4ab3
kawser <mkawserm@gmail.com>
2021-10-21 22:00:10 +0600

    working on new errors package

--- errors/errors_test.go
+++ errors/errors_test.go
@@ -1,9 +1,7 @@
 package errors
 
-import "testing"
-
-var ErrTest = New(1, "T_", "failed", false, map[string]string{})
-
-func TestError_ProtoErrorWithStack(t *testing.T) {
-	t.Logf("%+v\n", ErrTest.ProtoErrorWithStack())
-}
+//var ErrTest = New(1, "T_", "failed", false, map[string]string{})
+//
+//func TestError_ProtoErrorWithStack(t *testing.T) {
+//	t.Logf("%+v\n", ErrTest.ProtoErrorWithStack())
+//}
