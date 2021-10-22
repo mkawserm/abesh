8f5c0eca3c7cd59b42c0fb37db1c9fa4dc8d7739
kawser <mkawserm@gmail.com>
2021-10-22 22:53:44 +0600

    status updated with new helper functions

--- model/status.go
+++ model/status.go
@@ -9,3 +9,33 @@ func NewStatus(code uint32, prefix string, message string, params map[string]str
 		Params:  params,
 	}
 }
+
+// CloneStatus clone status to new status with new params
+func CloneStatus(status *Status, params map[string]string) *Status {
+	return &Status{
+		Code:    status.Code,
+		Prefix:  status.Prefix,
+		Message: status.Message,
+		Params:  params,
+	}
+}
+
+// CloneStatusWithMergedParams clone status to new status with new params merged
+func CloneStatusWithMergedParams(status *Status, params map[string]string) *Status {
+	p := make(map[string]string)
+
+	for k, v := range status.Params {
+		p[k] = v
+	}
+
+	for k, v := range params {
+		p[k] = v
+	}
+
+	return &Status{
+		Code:    status.Code,
+		Prefix:  status.Prefix,
+		Message: status.Message,
+		Params:  p,
+	}
+}
