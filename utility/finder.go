461b0aa9890c06dc475fcdc8f602630e96b01262
kawser <mkawserm@gmail.com>
2021-10-23 20:18:43 +0600

    comment updated for IsIn

--- utility/finder.go
+++ utility/finder.go
@@ -2,7 +2,7 @@ package utility
 
 import "sort"
 
-// IsIn find value in dataList.
+// IsIn finds value in the dataList.
 // dataList must be sorted
 func IsIn(dataList []string, value string) bool {
 	return sort.SearchStrings(dataList, value) != len(dataList)
